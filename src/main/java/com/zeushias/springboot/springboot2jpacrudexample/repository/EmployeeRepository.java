d063d9e190acf67c2217efb9c277804122605fa4
Zeushias <zeushias89@gmail.com>
2019-11-07 11:35:02 +0100

    Premier commit

---
+++ src/main/java/com/zeushias/springboot/springboot2jpacrudexample/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.zeushias.springboot.springboot2jpacrudexample.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.zeushias.springboot.springboot2jpacrudexample.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+	
+	
+}
